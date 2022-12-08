49d231c478fe151272b2cd210b392b91fe51cb91
happyman00095 <120114424+happyman00095@users.noreply.github.com>
2022-12-08 20:15:03 +0530

    first file

---
+++ array.js
@@ -0,0 +1,12 @@
+//var colors = ["blue","black","red","green"]
+//console.log(colors[2])
+
+var bike = [
+    {name : "mt15", color : "black", model_number : 5572},
+    {name : "r15", color : "black", model_number : 7854}
+]
+var length = bike.length
+
+console.log(bike[0].name)
+console.log(bike[0])
+console.log(length)
